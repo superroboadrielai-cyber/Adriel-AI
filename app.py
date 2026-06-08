739586e7d2104ea6343c89c1e804ad1a182dd4b6
superroboadrielai-cyber <superroboadrielai@gmail.com>
2026-06-08 15:04:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,2 @@
 import streamlit as st
-
-st.set_page_config(layout="wide")
-st.title("Sistema Adriel AI Pro")
-st.write("Bem-vindo ao painel principal.")
+st.set_page_config(page_title="Seu Nome Aqui", layout="wide")
